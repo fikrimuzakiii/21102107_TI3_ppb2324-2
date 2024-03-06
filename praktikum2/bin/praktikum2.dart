28387fdfcbcfa3ec751929682a023d6078a6dc44
mujek <21102107@ittelkom-pwt.ac.id>
2024-03-06 17:37:47 +0700

    Praktikum 2

---
+++ praktikum2/bin/praktikum2.dart
@@ -0,0 +1,56 @@
+void main() {
+  // print('Hello, World!');
+
+  // var name = 'Puspita';
+  // var year = 2023;
+  // var bulan = ['Januari', 'Februari', 'Maret', 'April'];
+
+  // print('$name, $year, $bulan');
+
+  // String greetings = 'Hello Dart!';
+  // int years = 2023;
+
+  // print('$greetings, $years');
+
+  int angka = 10;
+  if (angka > 0) {
+    print('bilangan positif');
+  } else if (angka < 0) {
+    print('bilangan negatif');
+  } else {
+    print('bilangan nol');
+  }
+
+  // String variable = 'value1';
+
+  // switch (variable) {
+  //   case 'value1':
+  //     print('This is value1');
+  //     break;
+  //   case 'value2':
+  //     print('This is value2');
+  //     break;
+  //   default:
+  //     print('This is a default value');
+  // }
+
+  // for (int i = 1; i <= 100; i++) {
+  //   print(i);
+  // }
+
+  // var i = 1;
+  // while (i <= 100) {
+  //   print(i);
+  //   i++;
+  // }
+
+  // var i = 1;
+  // do {
+  //   print(i);
+  //   i++;
+  // } while (i <= 100);
+
+  // List<int> numberList = [1, 2, 3, 4, 5];
+
+  // List dynamicList = [1, 'Informatika', true]; // List<dynamic>
+}
