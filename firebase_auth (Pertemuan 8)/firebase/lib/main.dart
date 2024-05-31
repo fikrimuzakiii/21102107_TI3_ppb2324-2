f769adb7926c953425277157d105be2c8bcde16b
mujek <21102107@ittelkom-pwt.ac.id>
2024-05-31 08:12:03 +0700

    Praktikum 10

--- firebase_auth (Pertemuan 8)/firebase/lib/main.dart
+++ firebase_auth (Pertemuan 8)/firebase/lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:firebase/ui/home_screen.dart';
+import 'package:firebase/ui/login.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -31,7 +34,24 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         navigatorKey: NAV_KEY,
         onGenerateRoute: generateRoute,
-        home: SplashScreen(),
+        home: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (snapshot.hasData) {
+              return HomeScreen();
+            } else if (snapshot.hasError) {
+              return const Center(
+                child: Text('Something went wrong'),
+              );
+            } else {
+              return const LoginScreen();
+            }
+          },
+        ),
       ),
     );
   }
